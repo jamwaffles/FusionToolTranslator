8d787644a69a3a37afc6c2fac9b31abe2053b92e
James Waples <jamwaffles@gmail.com>
2016-02-05 14:16:26 +0000

    Read file path from args

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,20 @@ use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
+use std::env;
 
 fn main() {
-	let path = Path::new("./tools.json");
+	let path = env::args().last().unwrap();
 
-	let mut f = match File::open("./tools.json") {
-		Err(e) => panic!("Unable to open {}: {}", path.display(), Error::description(&e)),
+	let mut f = match File::open(&path) {
+		Err(e) => panic!("Unable to open {}: {}", path, Error::description(&e)),
 		Ok(file) => file,
 	};
 
 	let mut fusionToolsString = String::new();
 
 	match f.read_to_string(&mut fusionToolsString) {
-		Err(e) => panic!("Unable to read {}: {}", path.display(), Error::description(&e)),
+		Err(e) => panic!("Unable to read {}: {}", path, Error::description(&e)),
 		Ok(string) => string,
 	};
 
