01e48fecf2d945285e4d22bbedd3d07933541d4a
James Waples <jamwaffles@gmail.com>
2016-02-05 14:05:45 +0000

    Read and print input file test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate rustc_serialize;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+	let path = Path::new("./tools.json");
+
+	let mut f = match File::open("./tools.json") {
+		Err(e) => panic!("Unable to open {}: {}", path.display(), Error::description(&e)),
+		Ok(file) => file,
+	};
+
+	let mut fusionToolsString = String::new();
+
+	match f.read_to_string(&mut fusionToolsString) {
+		Err(e) => panic!("Unable to read {}: {}", path.display(), Error::description(&e)),
+		Ok(string) => string,
+	};
+
+	print!("{}", fusionToolsString);
 }
