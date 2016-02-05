b32f47cdc8fe418c0594d6f93fe15b1868ab1a5f
James Waples <jamwaffles@gmail.com>
2016-02-05 14:57:49 +0000

    Parse and print tool JSON

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rustc_serialize;
+use rustc_serialize::json::Json;
 
 use std::error::Error;
 use std::fs::File;
@@ -9,16 +10,25 @@ fn main() {
 	let path = env::args().last().unwrap();
 
 	let mut f = match File::open(&path) {
-		Err(e) => panic!("Unable to open {}: {}", path, Error::description(&e)),
 		Ok(file) => file,
+		Err(e) => panic!("Unable to open {}: {}", path, Error::description(&e)),
 	};
 
 	let mut fusion_tools_string = String::new();
 
 	match f.read_to_string(&mut fusion_tools_string) {
-		Err(e) => panic!("Unable to read {}: {}", path, Error::description(&e)),
 		Ok(string) => string,
+		Err(e) => panic!("Unable to read {}: {}", path, Error::description(&e)),
 	};
 
 	print!("{}", fusion_tools_string);
+
+	let parsed_file = match Json::from_str(&fusion_tools_string) {
+		Ok(parsed) => parsed,
+		Err(e) => panic!("Invalid/empty JSON: {}", Error::description(&e)),
+	};
+
+	let fusion_tools = parsed_file.find_path(&[ "data" ]).unwrap().as_array().unwrap();
+
+	print!("{:?}", fusion_tools);
 }
