5941f1a7d2cbdaab48bc344ed584e2f60fc9accb
James Waples <jamwaffles@gmail.com>
2016-02-05 16:24:00 +0000

    Added cutter type and nicer debug formatting

--- src/fusion_tool.rs
+++ src/fusion_tool.rs
@@ -5,13 +5,13 @@ use std::fmt;
 #[derive(RustcDecodable, Debug)]
 pub struct FusionTool {
 	pub number: u16,
-	pub pocket: u16,
 	pub description: String,
+	pub cutter_type: String,
 	pub diameter: f32,
 }
 
 impl fmt::Display for FusionTool {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "Tool #{}\n    Number {}\n    {}\n    {}mm diameter", self.number, self.number, self.description, self.diameter)
+		write!(f, "#{}\t{}mm\t{} ({})", self.number, self.diameter, self.description, self.cutter_type)
 	}
 }
